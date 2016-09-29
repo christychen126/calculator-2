4fd8ddb6cf3d3ab2113c0b255b54d9512ad50855
Christy Chen <christychen126@gmail.com>
2016-09-29 08:06:44 -0700

    Add comments and Docstring

--- calculator.py
+++ calculator.py
@@ -8,7 +8,14 @@ calculator program yourself in this file.
 # import all functions from arithmetic module
 from arithmetic import *
 
+
 def process_input(input_string):
+    """ Process the string inputed by user and return operator and number list in a tuple
+
+        Split the string inputed by user into a list. Define the first item in the list as operator. 
+        Turn the rest of the items into integers and pack into a string with map function. 
+        Return the operator and the number list as a touple
+        """ 
     tokens = input_string.split()
     operator = tokens[0]
     numlist = map(int, tokens[1:])
@@ -19,6 +26,7 @@ def process_input(input_string):
 while True:
     # reads user input
     input_string = raw_input("> ")
+    #unpack tuple from process_input function
     operator, numlist = process_input(input_string)
 
     # responses to different user inputs
